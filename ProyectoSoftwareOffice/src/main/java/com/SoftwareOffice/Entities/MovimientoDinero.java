f338abf64491b18bcf0851f0fc6f36ec6fcc5e90
ingelect <alexisssoriano@hotmail.com>
2022-09-07 23:45:01 -0500

    ok

---
+++ ProyectoSoftwareOffice/src/main/java/com/SoftwareOffice/Entities/MovimientoDinero.java
@@ -0,0 +1,4 @@
+package com.SoftwareOffice.Entities;
+
+public class MovimientoDinero {
+}
---
+++ ProyectoSoftwareOffice/src/test/java/com/SoftwareOffice/ProyectoSoftwareOffice/ProyectoSoftwareOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareOffice.ProyectoSoftwareOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSoftwareOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

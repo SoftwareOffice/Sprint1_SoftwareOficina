e3bfbfa0bbbb7db385ccfe96520c22400735c049
ingelect <alexisssoriano@hotmail.com>
2022-09-08 22:08:58 -0500

    Se creo el ServicioEmpresa

---
+++ ProyectoSoftwareOffice/src/main/java/com/SoftwareOffice/Services/ServicioEmpresa.java
@@ -0,0 +1,16 @@
+package com.SoftwareOffice.Services;
+
+import com.SoftwareOffice.Entities.Empresa;
+
+public class ServicioEmpresa {
+    public void getnombre() {
+    }
+
+    public Empresa getEmpresa() {
+        return this.getEmpresa();
+    }
+
+    public Empresa getNombre() {
+        return null;
+    }
+}

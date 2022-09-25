2e3d2e765ae4d8941414473549bd10945ce52f5f
Juan Gonzalez <juanchoes.gon@gmail.com>
2022-09-24 22:30:15 -0500

    todo

---
+++ Enterprise/src/test/java/com/softwareOffice/Enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareOffice.Enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
